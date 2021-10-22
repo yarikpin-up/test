59fba214577437e1376b8a87506d7ea30c804d9b
yarikpin-up <92973833+yarikpin-up@users.noreply.github.com>
2021-10-22 17:47:27 +0300

    Update autoTest.js

--- test/specs/autoTest.js
+++ test/specs/autoTest.js
@@ -5,26 +5,41 @@ describe('TaskTest', () => {
     it('should login with valid credentials', async () => {
 
         await console.log('openPage CasinoPage');
-        await browser.url ("https://accounts.google.com/signin/v2/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
+        await browser.url ("https://docs.google.com/spreadsheets/d/1wV_b5TnfUw-9s-KzqhTHluDfVMkb2lQTLNUfty0ufnw/edit#gid=0");
         await browser.maximizeWindow();
 
 
         const Email = await $("#identifierId");
         await Email.setValue("autotestqa122@gmail.com"); //вводим пошту для входу
 
+        //await browser.debug();
 
         const EmailButton = await $("#identifierNext > div > button > span");
         await EmailButton.click();
 
-        const Password = await $("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input");
-        await Password.setValue("AutotestqA122"); //пароль от пошты
+        //await browser.debug();
 
-        const PasswordButton = await $("#passwordNext > div > button > span");
-        await PasswordButton.click();
+        const RepeatTryButton = await $("//*[@id='next']/div/button/span");
+        await RepeatTryButton.click();
 
-        const TitleLine = await $("#docs-title-widget > input")
+        //await browser.debug();
+        const Email2 = await $("#identifierId");
+        await Email2.setValue("autotestqa122@gmail.com"); //вводим пошту для входу
 
-        await assert.deepStrictEqual(await TitleLine.getValue(), "Table for autotesting");
+        //await browser.debug();
+
+        const RepeatTryButton2 = await $("//*[@id='identifierNext']/div/button");
+        await RepeatTryButton2.click();
+
+        const Info = await $("//*[@id='view_container']/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div/div[1]/a");
+        await Info.click();
+
+        //await browser.debug();
+
+        const TitleLine = await $("/html/body/div[2]/div/section/div/div[1]/article/section/h1");
+        const Title = await TitleLine.getText()
+
+        await assert.deepStrictEqual(Title, "Как войти в аккаунт в поддерживаемом браузере");
 
     });
 });
